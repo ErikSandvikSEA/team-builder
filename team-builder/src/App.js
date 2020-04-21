3637f4e4c53349d95e779bdffcf18cd07b7f56ef
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-21 13:07:41 -0700

    added form and teamMember functionality

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid' // GROSS
+import Form from './Form'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const initialTeamMembersList = [
+  {
+    id: uuid(),
+    fName: 'First Name',
+    lName: 'Last Name',
+    position: 'Position',
+    height: 'Height',
+    number: 'Number',
+    allStar: false,
+  },
+]
+
+const initialFormValues = {
+  fName: '',
+  lName: '',
+  position: '',
+  height: '',
+  number: '',
+  allStar: 'no',
+}
+
+
+const App = () => {
+  const [teamMembers, setTeamMembers] = useState(initialTeamMembersList)
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const onInputChange = evt => {
+    // 🔥 STEP 4 - IMPLEMENT A CHANGE HANDLER (works for inputs and dropdowns)
+    // which can change the state of inputs of type text
+
+    // a) pull the name of the input from the event object
+    const inputName = evt.target.name
+   // either 'username' or 'email'
+    // b) pull the value of the input from the event object
+    const inputValue = evt.target.value
+ // who knows, the current value
+    // c) set a new state for the whole form // ARGH
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='container'>
+    <header><h1>Team Members App</h1></header>
+    {/* {
+      teamMembers.map(teamMember => {
+        return (
+          <TeamMember key={teamMember.id} details={teamMember} />
+        )
+      })
+    } */}
+
+    <Form
+      // 🔥 STEP 2 - THE FORM WANTS ITS FOOD!!!!
+      // check implementation of FriendForm
+      // to see what props it expects
+      values={formValues}
+      onInputChange={onInputChange}
+    />
+  </div>
+  )
 }
 
 export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+function Form(props) {
+     // THESE ARE THE **EXACT PROPS** FriendForm EXPECTS!
+     const {
+          values,
+          onInputChange,
+     } = props
+
+     return (
+          <form className='friend container'>
+               <h2>Team Member Form</h2>
+               {/* ////////// TEXT INPUTS ////////// */}
+               <label>First Name:&nbsp;
+       <input
+                         // 🔥 STEP 3A - THIS INPUT WANTS VALUE & CHANGE HANDLER!
+                         // Inputs render what they're told
+                         // Their current value ultimately comes from app state
+                         // At each keystroke, a change handler should fire
+                         value={values.fName}
+                         onChange={onInputChange}
+                         name='fName'
+                         type='text'
+                    /></label>
+
+               <label>Last Name:&nbsp;
+       <input
+                         // 🔥 STEP 3A - THIS INPUT WANTS VALUE & CHANGE HANDLER!
+                         // Inputs render what they're told
+                         // Their current value ultimately comes from app state
+                         // At each keystroke, a change handler should fire
+                         value={values.lName}
+                         onChange={onInputChange}
+                         name='lName'
+                         type='text'
+                    /></label>
+
+               <label>Position:&nbsp;
+       <input
+                         // 🔥 STEP 3B - THIS INPUT WANTS VALUE & CHANGE HANDLER!
+                         value={values.position}
+                         onChange={onInputChange}
+                         name='position'
+                         type='text'
+                    /></label>
+
+               <label>Height:&nbsp;
+       <input
+                         // 🔥 STEP 3B - THIS INPUT WANTS VALUE & CHANGE HANDLER!
+                         value={values.height}
+                         onChange={onInputChange}
+                         name='height'
+                         type='text'
+                    /></label>
+
+               {/* ////////// DROPDOWN ////////// */}
+               <label>All Star&nbsp;
+       <select
+                         // very similar to inputs of type text
+                         value={values.allStar}
+                         onChange={onInputChange}
+                         name='allStar'
+                    >
+                         <option value='no'>Not Voted as an All Star</option>
+                         <option value='yes'>Voted as an All Star</option>
+                    </select></label>
+
+          </form>
+     )
+}
+
+export default Form
